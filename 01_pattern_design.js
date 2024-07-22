070e402992a33c4cfb59bc375a34f491cf3d794e
Zamil Akhter <zamilakhter.nwl@gmail.com>
2024-07-22 13:12:00 +0530

    Added a js new code

--- 01_pattern_design.js
+++ 01_pattern_design.js
@@ -127,8 +127,6 @@ for(let i=0; i<n; i++){
     for(let j=0; j<n-i-1; j++){
         space += " ";
     }
-    // space += "A";
-    // console.log(space);
     for(let k=0; k<2*i+1; k++){
         space +="A"
     }
