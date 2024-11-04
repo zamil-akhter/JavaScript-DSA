7081642b3dbfd5de5fdaa6bfc2c0bb190f453f5e
Zamil Akhter <zamilakhter.nwl@gmail.com>
2024-11-04 12:10:28 +0530

    logic changed

--- 02_string_problem.js
+++ 02_string_problem.js
@@ -2,26 +2,20 @@ let string = " are6 you5 sh1ow me2 good100 how487 "
 // o/p:- "show me you are good how"
 
 let stringWithPosition = {};
+let stringArray = string.trim().split(' ')
 
-let stringArray = string.split(' ')
 for(let i=0; i<stringArray.length; i++){
-    if(stringArray[i].length === 0){
-        stringArray.splice(i,1)
-    }
-    if(stringArray[i] !== undefined){
-        let strValue = ''
-        let keyValue = ''
-        for(let j=0; j<stringArray[i].length; j++){
-            if(stringArray[i][j] >= 'a' && stringArray[i][j] <= 'z'){
-                strValue += stringArray[i][j]
-            }else{
-                keyValue +=stringArray[i][j]
-            }
-            if(j === stringArray[i].length-1){
-                stringWithPosition[keyValue] = strValue
-            }
+    let strValue = ''
+    let keyValue = ''
+    for(let j=0; j<stringArray[i].length; j++){
+        if(stringArray[i][j] >= 'a' && stringArray[i][j] <= 'z'){
+            strValue += stringArray[i][j]
+        }else{
+            keyValue +=stringArray[i][j]
         }
-
+        if(j === stringArray[i].length-1){
+            stringWithPosition[keyValue] = strValue
+        }
     }
 }
 let outputArray = Object.values(stringWithPosition);
